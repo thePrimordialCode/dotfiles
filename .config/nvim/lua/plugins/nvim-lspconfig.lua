f13e2920f4f0974f768a932df5a2130dcf5af94b
thePrimordialCode <primordialxcode@gmail.com>
2025-02-21 06:17:24 +0000

    rust_analyzer lua_ls

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,21 @@ return {
 
   opts = {
     servers = {
+      lua_ls = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+        },
+      },
+      rust_analyzer = {
+        settings = {
+          rust_analyzer = {
+            diagnostics = {
+              enable = true,
+            },
+          },
+        },
+      },
       basedpyright = {
         settings = {
           basedpyright = {
