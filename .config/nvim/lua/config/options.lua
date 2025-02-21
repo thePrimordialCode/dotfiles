3398ce45e5ede31c3631553f9c9aa537e4548b00
thePrimordialCode <primordialxcode@gmail.com>
2025-02-21 06:18:33 +0000

    default root selection

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 
 -- vim.opt.mouse = ""
-vim.g.root_spec = { "cwd" } -- Set root directory to current one
+-- vim.g.root_spec = { "cwd" } -- Set root directory to current one
 vim.opt.termguicolors = true -- Ensure true color support
 vim.opt.winblend = 0 -- Disable winblend
 vim.opt.winhighlight = "" -- Clear any winhighlight settings
